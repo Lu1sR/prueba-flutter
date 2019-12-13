19b726fd169fbdabe0f4af57e022e6b40c8cce2b
Luis Rodriguez <luishrt1997@gmail.com>
2019-12-13 09:57:34 -0800

    primer

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
